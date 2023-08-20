974ca78d58ca55ccaf356c95155571024588bbef
Ankit Raj <ankitraj21475@gmail.com>
2023-08-20 16:38:08 +0530

    heading color change

--- src/theme.jsx
+++ src/theme.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider, createGlobalStyle } from 'styled-components';
 const theme = {
   fontFamily: 'Roboto, sans-serif',
   mainColors: {
-    blue: '#2400ff',
+    blue: '#03001C',
     gray: '#c6c6c6',
     dark: '#03001C',
   },
